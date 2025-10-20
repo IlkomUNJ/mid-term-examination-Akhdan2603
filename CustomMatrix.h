ac2c49d7612f40d7c84d890309aef828ac9af1c2
Akhdan2603 <151690460+Akhdan2603@users.noreply.github.com>
2025-10-20 15:23:33 +0700

    p
    Removed commented-out code and fixed the end of the header guard.

--- CustomMatrix.h
+++ CustomMatrix.h
@@ -12,12 +12,10 @@ public:
 
     }
 
-    //alternate way to instantiate the class
     CustomMatrix(bool m[3][3]){
         const int m_size = sizeof(m) / sizeof(m[0]);
         for (int i=0;i< m_size;i++) {
             for(int j=0; j < sizeof(m[0]);j++){
-                //cout << i << " " << j << endl;
                 mat[i][j] = m[i][j];
             }
         }
@@ -27,11 +25,10 @@ public:
         const int m_size = sizeof(m) / sizeof(m[0]);
         for (int i=0;i< m_size;i++) {
             for(int j=0; j < sizeof(m[0]);j++){
-                //cout << i << " " << j << endl;
                 mat[i][j] = m[i][j];
             }
         }
     }
 };
 
-#endif // CUSTOMMATRIX_H
+#endif
